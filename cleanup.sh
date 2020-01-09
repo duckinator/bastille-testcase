0c08bb8a0701ccc0a7b65648e3779ccfa6949426
Ellen Marie Dash <me@duckie.co>
2020-01-09 11:14:51 -0500

    Add cleanup.sh script.

---
+++ cleanup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+ls /usr/local/bastille/jails/* 2>/dev/null || { echo "No remaining containers."; exit; }
+
+for container in $(ls /usr/local/bastille/jails/* 2>/dev/null | cut -d '/' -f 6); do
+	bastille stop $container
+	bastille destroy $container
+done
+
+echo
+echo
+echo
+
+ls /usr/local/bastille/jails/* 2>/dev/null || { echo "All containers removed."; exit; }
+
+echo "Containers that could not be stopped+destroyed properly:"
+
+for container in $(ls /usr/local/bastille/jails/* 2>/dev/null | cut -d '/' -f 6); do
+	echo "- $container"
+done
+
+echo
+echo
+ANSWER="x"
+while [ "$ANSWER" != "y" ] && [ "$ANSWER" != "Y" ] && [ "$ANSWER" != "n" ] && [ "$ANSWER" != "N" ]; do
+	read -p "Forcefully clean up? [Y/n] " ANSWER
+done
+
+if [ "$ANSWER" == "n" ] || [ "$ANSWER" == "N" ]; then
+	echo "Aborting."
+	exit
+fi
+
+chflags noschg /usr/local/bastille/jails/*/root/var/empty
+rm -r /usr/local/bastille/jails/*
